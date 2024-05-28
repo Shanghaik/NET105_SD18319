c4be9be9d265242e6e9f8b8f69a9e965ebf6f568
shanghaik168@gmail.com <Shanghaik1998>
2024-05-28 17:18:01 +0700

    callAPI với HttpClient

---
+++ AppAPI/Controllers/TreeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TreeController : ControllerBase
+    {
+        [HttpGet("get-fake-tree")]
+        public ActionResult GetFakeTree()
+        {
+            List<Tree> tree = new List<Tree>()
+            {
+                new Tree() {Name = "KICM", Description = "Nhạc sỹ liêm khiết"},
+                new Tree() {Name = "JJK", Description = "Hội 1/2"},
+                new Tree() {Name = "Cây phấn đào", Description = "Mọc ở chân đèo"}
+            };
+            return Ok(tree);
+        }
+        [HttpPost("get-tree-by-name")] // Cái này viết ra để mình test và nhớ thôi/ đừng thử 
+        public ActionResult GetTreeDetails(string name, List<Tree> trees)
+        {
+            Tree tree = trees.FirstOrDefault(x => x.Name == name);
+            return Ok(tree);
+        }
+    }
+    public class Tree
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ TestAPIWeb/Controllers/TreeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace TestAPIWeb.Controllers
+{
+    public class TreeController : Controller
+    {
+        public IActionResult Index() // Lấy ra cái danh sách tree và hiển thị ra trên
+        {
+            // Bước 1: Lấy requestURL
+            string requestURL = @"https://localhost:7162/api/Tree/get-fake-tree";
+            // Bước 2: Lấy response
+            HttpClient client= new HttpClient();
+            var response = client.GetStringAsync(requestURL).Result;
+            // Bước 3: ÉP kiểu nếu cần (DÙng thư viện Newtonsoft.Json để ép từ jsonString -> List
+            List<Tree> trees = JsonConvert.DeserializeObject<List<Tree>>(response);
+            // Bước 4: Sử dụng data
+            return View(trees);
+        }
+    }
+    public class Tree
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
